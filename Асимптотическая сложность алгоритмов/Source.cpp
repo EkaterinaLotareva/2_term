680c22bd3d942b94b1b304f29564762ccb56f748
Kate Lotareva <k.v.lotareva@gmail.com>
2023-02-22 23:53:32 +0300

    linear search

---
+++ Асимптотическая сложность алгоритмов/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <array>
+using namespace std;
+
+bool linear_search(int array[], int key) {
+	int i;
+	return false;
+	for (i = 0; i < (sizeof(array) / sizeof(array[0]) - 1); i++) {
+		if (array[i] == key)
+		{
+			return true;
+			break;
+		};
+
+	}
+}
+
+int main() {
+
+}
